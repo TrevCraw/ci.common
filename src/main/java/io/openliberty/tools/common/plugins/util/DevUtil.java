4b3e685d7cf993082f6e776f21e9ca021e413fc7
Trevor Crawford <tcrawfo6@uwo.ca>
2020-07-13 13:35:16 -0400

    Check if liberty-plugin-variable-config.xml is already mounted

--- src/main/java/io/openliberty/tools/common/plugins/util/DevUtil.java
+++ src/main/java/io/openliberty/tools/common/plugins/util/DevUtil.java
@@ -1090,19 +1090,24 @@ public abstract class DevUtil {
         command.append(" -v " + serverDirectory + "/apps:/config/apps");
         command.append(" -v " + serverDirectory + "/dropins:/config/dropins");
 
-        // mount the loose application resources in the container
-        command.append(" -v " + projectDirectory.getAbsolutePath() + ":" + DEVMODE_DIR_NAME);
-        //TODO: add check to see if this file was already lined up to be mounted from being in the Dockerfile
-        command.append(" -v " + serverDirectory.getAbsolutePath() + "/configDropins/overrides/liberty-plugin-variable-config.xml" +
-                       ":/config/configDropins/overrides/liberty-plugin-variable-config.xml");
-
-        // mount the server logs directory over the /logs used by the open liberty container as defined by the LOG_DIR env. var.
-        command.append(" -v " + serverDirectory.getAbsolutePath() + "/logs:/logs");
-
+        boolean generatedConfigMounted = false;
         // mount all files from COPY commands in the Dockerfile to allow for hot deployment
         for (int i=0; i < srcMount.size(); i++) {
             command.append(" -v " + srcMount.get(i) + ":" + destMount.get(i));
+            if (srcMount.get(i).endsWith("liberty-plugin-variable-config.xml")) {
+                generatedConfigMounted = true;
+            }
         }
+
+        // mount the loose application resources in the container
+        command.append(" -v " + projectDirectory.getAbsolutePath() + ":" + DEVMODE_DIR_NAME);
+        if (!generatedConfigMounted) {
+            command.append(" -v " + serverDirectory.getAbsolutePath() + "/configDropins/overrides/liberty-plugin-variable-config.xml" +
+                           ":/config/configDropins/overrides/liberty-plugin-variable-config.xml");
+        }
+
+        // mount the server logs directory over the /logs used by the open liberty container as defined by the LOG_DIR env. var.
+        command.append(" -v " + serverDirectory.getAbsolutePath() + "/logs:/logs");
 
         command.append(" --name " + DEVMODE_CONTAINER_NAME);
 
